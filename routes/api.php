5e887c7adf8fe8577d673a38f2601a1bc7d68ed1
Muhid <muhid@skylarksoft.com>
2021-05-25 15:29:23 +0600

    Added vue

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('api')->group(function () {
-    Route::resource('products', ProductController::class);
-});
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('api')->group(function () {
+    Route::resource('products', ProductController::class);
 });
