08f591e60aa38ab89463c348661c52d4af2537bf
Muhid <muhid@skylarksoft.com>
2021-05-25 11:42:12 +0600

    Added facker

--- app/Product.php
+++ app/Product.php
@@ -1,12 +1,10 @@
 <?php
 
 namespace App;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    use HasFactory;
     protected $fillable = [
         'name',
         'detail'
